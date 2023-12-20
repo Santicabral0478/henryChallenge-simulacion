3997af6706d8caba3174b1e5ac2c43faf727ee1a
Santicabral0478 <scsantiagocabral@gmail.com>
2023-12-20 17:45:58 -0300

    simulacion completa

---
+++ 01.js
@@ -0,0 +1,30 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function soloNumeros(array) {
+  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+  // Debe devolver un arreglo con solo los enteros.
+  // Ej: 
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+
+  // Tu código aca:
+
+  var enterosSeparados = [];
+
+  for(var i = 0; i<array.length; i++){
+    if(Number.isInteger(array[i])){
+      enterosSeparados.push(array[i]);
+    }
+  }
+
+  return enterosSeparados;
+
+}
+
+// No modifiques nada debajo de esta linea //
+
+
+module.exports = soloNumeros
